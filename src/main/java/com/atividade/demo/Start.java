36e6c6cb2ca591211887108e12849d6edf6b21fd
Fabio Rodrigues da Silva <frsilva2101@gmail.com>
2022-10-24 00:44:50 -0300

    Implementando css para pagina index e livros por categoria

--- src/main/java/com/atividade/demo/Start.java
+++ src/main/java/com/atividade/demo/Start.java
@@ -9,5 +9,5 @@ public class Start {
 	@RequestMapping("/index")
 	public ModelAndView index() {
 		return new ModelAndView("index");
-	}
+	}                                                                                                 
 }
