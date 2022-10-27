14ebfea7ee8ae10a29b319d8b63a72fc6546d84f
Auto Logon <autologon@CSED.LAB>
2022-10-27 20:16:32 -0300

    preparando para mudar a conexão do banco para jpa

---
+++ src/main/java/atividade/jdbc/Connection.java
@@ -0,0 +1,5 @@
+package atividade.jdbc;
+
+public class Connection {
+
+}
--- src/main/java/com/atividade/demo/TbCategoria.java
+++ src/main/java/com/atividade/demo/TbCategoria.java
@@ -4,8 +4,8 @@ public class TbCategoria {
 	private String Arquitetura;
 	private String 	Desgin;
 	private String 	Informática;
-	private String 'Literatura brasileira';
-	private String 'Literatura infantil';
-	private String 'Literatura universal';
-	private String 'Música e arte';
+	private String Literatura_brasileira;
+	private String Literatura_infantil;
+	private String Literatura_universal;
+	private String Música_e_arte;
 }
