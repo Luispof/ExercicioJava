e3de8a7479ce4eb844beff248f30f21445111906
Auto Logon <autologon@CSED.LAB>
2022-10-26 21:12:27 -0300

    Adicionado o CRUD

--- src/main/java/com/atividade/demo/Banco.java
+++ src/main/java/com/atividade/demo/Banco.java
@@ -5,30 +5,52 @@ import atividade.jdbc.Connetion;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
 public class Banco {
 	private Connection connection;
+
 	public Banco() {
 		this.connection = new Connetion().getConnetion();
 	}
 
-
-public void adicionar(Tblivros tblivro) {
-	String sql = "inset into tblivros"+
-				 "(titulo, autor, categoria, valor)"+
-				 "values (?,?,?,?)";
-	try {
-		PreparedStatement stmt = connection.prepareStatement(sql);
-		stmt.setString(1, tblivro.getTitulo());
-		stmt.setString(2, tblivro.getAutor());
-		stmt.setString(3, tblivro.getCategoria());
-		stmt.setFloat(4, tblivro.getValor());
-		
-		stmt.execute();
-		stmt.close();
-	}catch(SQLException e) {
-		throw new RuntimeException(e);
+	public void adicionar(Tblivros tblivro) {
+		String sql = "inset into tblivros" + "(titulo, autor, categoria, valor)" + "values (?,?,?,?)";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, tblivro.getTitulo());
+			stmt.setString(2, tblivro.getAutor());
+			stmt.setString(3, tblivro.getCategoria());
+			stmt.setFloat(4, tblivro.getValor());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
-}
+
+	public void update(Tblivros tblivro) {
+		String sql = "update tblivros set titulo=?, autor=?, categoria=?, valor=?" + "where codlivro={param.codl}";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, tblivro.getTitulo());
+			stmt.setString(2, tblivro.getAutor());
+			stmt.setString(3, tblivro.getCategoria());
+			stmt.setFloat(4, tblivro.getValor());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void remover(Tblivros tblivro) {
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete " + "from tblivros codlivro={param.codl}");
+			stmt.setInt(1, tblivro.getCodlivro());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
---
+++ src/main/java/com/atividade/demo/TbCategoria.java
@@ -0,0 +1,11 @@
+package com.atividade.demo;
+
+public class TbCategoria {
+	private String Arquitetura;
+	private String 	Desgin;
+	private String 	Informática;
+	private String 'Literatura brasileira';
+	private String 'Literatura infantil';
+	private String 'Literatura universal';
+	private String 'Música e arte';
+}
